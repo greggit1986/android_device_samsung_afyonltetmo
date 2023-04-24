286beae72708faa59fe7a80cc42fac541dfa6ae6
greg <greg.jwaerhouse@gmail.com>
2023-04-23 20:14:15 -0400

    Dirty-Experimenting with flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,10 +16,14 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8226
 TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 zcache.enabled=1 zcache.compressor=lz4
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK :=  $(DEVICE_TREE)/mkbootimg.mk
+
 # Kernel-prebuilt (comment all if using source)
 TARGET_PREBUILT_KERNEL := device/samsung/afyonltetmo/kernel
 BOARD_KERNEL_DT := device/samsung/afyonltetmo/dt.img
@@ -31,14 +35,22 @@ BOARD_MKBOOTIMG_ARGS += --dt $(BOARD_KERNEL_DT)
 #TARGET_KERNEL_SOURCE := kernel/samsung/msm8226
 #TARGET_KERNEL_CONFIG := twrp_afyonltetmo_defconfig
 #KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
-#BOARD_CUSTOM_BOOTIMG := true
-#BOARD_CUSTOM_BOOTIMG_MK :=  $(DEVICE_TREE)/mkbootimg.mk
+
 
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 4096
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A7DEA0
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 12562643968
+#BOARD_FLASH_BLOCK_SIZE := 4096
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A7DEA0
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 12562643968
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
+BOARD_CACHEIMAGE_PARTITION_SIZE := 721420288
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2569011200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12631588352
+
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -47,9 +59,15 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 BOARD_SUPPRESS_EMMC_WIPE := true
 
+ #JJADD
 BOARD_NEEDS_LZMA_MINIGZIP := true
-#BOARD_RAMDISK_USE_XZ := true
-LZMA_RAMDISK_TARGETS := recovery #JJADD
+BOARD_RAMDISK_USE_XZ := true
+LZMA_RAMDISK_TARGETS := recovery
+
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_ROOT_EXTRA_FOLDERS := efs firmware firmware-modem persist
+BOARD_ROOT_EXTRA_SYMLINKS := \
+    /data/tombstones:/tombstones
 
 # TWRP
 DEVICE_RESOLUTION := 540x960
@@ -70,8 +88,36 @@ BOARD_HAS_NO_REAL_SDCARD := true
 #TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,journal_async_commit,errors=panic wait,check,encryptable=footer"
 #TW_CRYPTO_FS_FLAGS := "0x00000406"
 #TW_CRYPTO_KEY_LOC := "footer"
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
+ #JJADD
+TW_DEFAULT_BRIGHTNESS := 167
+TW_MAX_BRIGHTNESS := 255
+
+export LC_ALL := C
+
+ #TARGET_RECOVERY_DEVICE_DIRS += device/samsung/afyonltetmo
+
+ #JJADD-Debug flags
+TARGET_USES_LOGD:= true
+#TARGET_EVENT_LOGGIND:= false
+TW_INCLUDE_LOGCAT := true
+#TARGET_RECOVERY_DEVICE_MODULES += debuggerd
+#TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT_EXECUTABLES)/debuggerd
+
+# Removes the 'Reboot bootloader' button
+TW_NO_REBOOT_BOOTLOADER := true
+# Removes the 'Reboot recovery' button
+TW_NO_REBOOT_RECOVERY := true
+# Disable the battery percentage for devices where it doesn't work properly
+#TW_NO_BATT_PERCENT := true
+# Same as above, for CPU Temperature
+#TW_NO_CPU_TEMP := true
+# Remove MTP support
+TW_USE_TOOLBOX := true
+
+
+
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
